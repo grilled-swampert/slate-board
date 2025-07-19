ef1d86921dc01514a7f52d2f05bf6d0ece007d2e
grilled-swampert <128573732+grilled-swampert@users.noreply.github.com>
2025-07-19 10:09:40 +0530

    cors added

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,9 @@ const limiter = rateLimit({
 
 const app = express();
 app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+}));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(limiter);
